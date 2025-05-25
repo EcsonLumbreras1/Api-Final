c02f12084c71d333a558edbafb8a83b1911986c4
Ecson Lumbreras <75402679+EcsonLumbreras1@users.noreply.github.com>
2025-05-25 15:08:47 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
 });
 
 // 🌐 Configurar puerto para Railway
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
